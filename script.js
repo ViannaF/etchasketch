63d625e19f1579027815ea457cefafd352a6c878
ViannaF <88505281+ViannaF@users.noreply.github.com>
2024-05-30 17:48:39 +0100

    Improve color functionality
    Change random colors to rgb.
    Prevent changing color on second hover

--- script.js
+++ script.js
@@ -2,39 +2,42 @@ const container = document.querySelector(".container");
 const resetBtn = document.querySelector(".reset");
 
 const createDivs = (numSquares) => {
-    for (let i = 0; i < numSquares * numSquares; i++) {
-        const div = document.createElement("div");
-        div.style.width = `calc(100%/${numSquares})`;
-        div.style.height = `calc(100%/${numSquares})`;
-        container.appendChild(div);
-        div.classList.add("inner");
-    }
-}
+  for (let i = 0; i < numSquares * numSquares; i++) {
+    const div = document.createElement("div");
+    div.style.width = `calc(100%/${numSquares})`;
+    div.style.height = `calc(100%/${numSquares})`;
+    container.appendChild(div);
+    div.classList.add("inner");
+  }
+};
 
 const clearContainer = () => {
-    container.replaceChildren();
-}
+  container.replaceChildren();
+};
 
 const defaultSize = 16;
 createDivs(defaultSize);
 
 container.addEventListener("mouseover", (event) => {
-    const randomColor = Math.floor(Math.random()*16777215).toString(16);
-    if (event.target.classList.contains("inner")) {
-        event.target.style.backgroundColor = "#" + randomColor;
-    }
+  if (!event.target.dataset.originalColor) {
+    const r = Math.floor(Math.random() * 256);
+    const g = Math.floor(Math.random() * 256);
+    const b = Math.floor(Math.random() * 256);
+    const randomColor = `rgb(${r},${g},${b})`;
+    event.target.dataset.originalColor = randomColor;
+    event.target.style.backgroundColor = randomColor;
+  }
 });
 
 resetBtn.addEventListener("click", () => {
-    const userInput = prompt("Please enter number of squares per side.");
-    const gridSize = parseInt(userInput);
-    
-    if(isNaN(gridSize) || gridSize <= 0 || gridSize > 100) {
-        alert("Please enter a valid number between 1 and 100.");
-        return;
-    };
+  const userInput = prompt("Please enter number of squares per side.");
+  const gridSize = parseInt(userInput);
 
-    clearContainer();
-    createDivs(gridSize);
+  if (isNaN(gridSize) || gridSize <= 0 || gridSize > 100) {
+    alert("Please enter a valid number between 1 and 100.");
+    return;
+  }
+
+  clearContainer();
+  createDivs(gridSize);
 });
-
