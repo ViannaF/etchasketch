67b418973e70f78cd8e292736a5f91fa5747e40a
ViannaF <88505281+ViannaF@users.noreply.github.com>
2024-05-30 21:06:39 +0100

    Add erase and tonal buttons

--- script.js
+++ script.js
@@ -1,5 +1,8 @@
 const container = document.querySelector(".container");
 const resetBtn = document.querySelector(".reset");
+const rainbow = document.querySelector(".rainbow");
+const tonal = document.querySelector(".tonal");
+const eraseBtn = document.querySelector(".erase");
 
 const createDivs = (numSquares) => {
   for (let i = 0; i < numSquares * numSquares; i++) {
@@ -23,12 +26,43 @@ const setRandomColor = (event) => {
   event.target.style.backgroundColor = randomColor;
 };
 
+const makeTonal = (event) => {
+  let opacity = parseFloat(event.target.style.opacity) || 0;
+  if (opacity < 1) {
+    opacity += 0.1;
+    event.target.style.opacity = opacity;
+  }
+  if (opacity >= 1) {
+    return;
+  }
+  event.target.style.backgroundColor = "rgba(0, 0, 0, " + opacity + ")";
+};
+
+const eraseCells = (event) => {
+  event.target.style.backgroundColor = "rgb(255, 255, 255)";
+}
+
 const defaultSize = 16;
 createDivs(defaultSize);
 
-container.addEventListener("mouseover", (event) => {
-  setRandomColor(event);
+rainbow.addEventListener("click", () => {
+  container.removeEventListener("mouseover", eraseCells);
+  container.removeEventListener("mouseover", makeTonal);
+  container.addEventListener("mouseover", setRandomColor);
 });
+
+tonal.addEventListener("click", () => {
+  container.removeEventListener("mouseover", eraseCells)
+  container.removeEventListener("mouseover", setRandomColor);
+  container.addEventListener("mouseover", makeTonal);
+});
+
+eraseBtn.addEventListener("click", () => {
+  container.removeEventListener("mouseover", makeTonal);
+  container.removeEventListener("mouseover", setRandomColor);
+  container.addEventListener("mouseover", eraseCells);
+
+})
 
 resetBtn.addEventListener("click", () => {
   const userInput = prompt("Please enter number of squares per side.", "16");
